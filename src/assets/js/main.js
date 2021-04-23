4b4d6e13a6313987faea20cb73b5ba01783a1687
Nataliya Rohochaya <lc4821577@gmail.com>
2021-04-23 15:20:53 +0300

    feature: rendering page desktop

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,18 @@
 //Swiper
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.electrical-swiper', {
     spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const mySwiper = new Swiper('.commercial-lighting-swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
