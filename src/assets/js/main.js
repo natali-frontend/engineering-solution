73add5bca72fd37cfa815bd0f26b32f51cccd2cf
Nataliya Rohochaya <lc4821577@gmail.com>
2021-04-27 11:37:59 +0300

    feature: add clients page desktop

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -18,3 +18,15 @@ const mySwiper = new Swiper('.commercial-lighting-swiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+const Myswiper = new Swiper('.work-swiper', {
+    slidesPerView: 2,
+    spaceBetween: 60,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
