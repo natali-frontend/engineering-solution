bf83cdb71a204f29c8e6037cec7995010cf1e20c
Nataliya Rohochaya <lc4821577@gmail.com>
2021-04-30 17:50:52 +0300

    feature: add drop down list

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,5 @@
-//Swiper
-const swiper = new Swiper('.electrical-swiper', {
-    spaceBetween: 30,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+const menuItemServices = document.getElementsByClassName('menu-item-services');
+const dropDownList = document.getElementsByClassName('drop-down-list');
 
 menuItemServices.addEventListener("click", function () {
     dropDownList.style.display = 'flex';
