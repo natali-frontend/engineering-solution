210ef44804e864dc04643fe0d67e4e870c3dc76f
Nataliya Rohochaya <lc4821577@gmail.com>
2021-06-16 12:59:09 +0300

    feature: add project electrical page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -36,3 +36,27 @@ const swiper = new Swiper('.testimonial-swiper', {
         }
     }
 });
+
+//Swiper
+const Myswiper = new Swiper('.work-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 15,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        767: {
+            spaceBetween: 0,
+            slidesPerView: 2,
+        },
+        991: {
+            spaceBetween: 0,
+            slidesPerView: 2,
+        }
+    }
+});
